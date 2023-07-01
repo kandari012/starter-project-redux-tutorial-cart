711816960a75f9c98814d68d24c14d08454eec34
rahul kandari <rahulkandari012@gmail.com>
2023-07-01 12:32:08 +0530

    adding first action

--- src/App.js
+++ src/App.js
@@ -6,6 +6,23 @@ import CartContainer from "./components/CartContainer";
 import cartItems from "./cart-items";
 // redux stuff
 
+import { createStore } from "redux";
+
+const initialState = {
+  count: 0,
+};
+
+const reducer = (state, action) => {
+  console.log({ state, action });
+  if (action.type === "DECREASE") {
+    return { ...state, count: state.count - 1 };
+  }
+  return state;
+};
+const store = createStore(reducer, initialState);
+store.dispatch({ type: "DECREASE" });
+console.log(store.getState());
+
 function App() {
   // cart setup
 
